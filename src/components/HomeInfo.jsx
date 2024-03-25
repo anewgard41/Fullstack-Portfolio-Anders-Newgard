75f4294f6940530ce3e4e022862946bffdeafdbf
Anders Newgard <anne9707@colorado.edu>
2024-03-25 17:04:52 -0400

    began formatting contact page

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -6,8 +6,9 @@ const InfoBox = ({ text, link, btnText }) => {
   return(
   <div className="info-box">
     <p className="font-medium sm:text-xl text-center">{text}</p>
-    <Link to={link} className="btn">
+    <Link to={link} className="neo-brutalism-white neo-btn">
       {btnText}
+      <img src={arrow} alt="arrow" className="w-4 h-4 object-contain"/>
     </Link>
   </div>
   )
@@ -21,17 +22,25 @@ const renderContent = {
     </h1>
   ),
   2: (
-    <InfoBox 
-    text = "I'm currently looking for a new role. If you're interested in working together, feel free to reach out!"
-    link = "/contact"
-    btnText = "Contact Me"
+    <InfoBox
+      text="Versatile Full Stack Developer with a passion for creating engaging and user-friendly applications. I have experience in multiple industries and am always eager to learn."
+      link="/about"
+      btnText="Learn More"
     />
   ),
   3: (
-    <InfoBox />
+    <InfoBox
+      text="Collaborated with multiple teams to develop and deploy accessible, high quality applications. Check out my projects to see some of my work!"
+      link="/projects"
+      btnText="Visit my portfolio"
+    />
   ),
   4: (
-    <InfoBox />
+    <InfoBox
+      text="Feel free to reach out to me with any questions or project ideas. I am always looking for excellent people with cool ideas to work with."
+      link="/contact"
+      btnText="Shoot me a message!"
+    />
   ),
 };
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const handleChange = (e = {});
 
-export default Contact
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Max"
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="max@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <input
+              type="message"
+              name="name"
+              rows={4}
+              className="textarea"
+              placeholder="How can I help you?"
+              required
+              value={form.message}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit" className="btn"></button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
