65e119afa591e10630b662db678b73589fe5a396
Anders Newgard <anne9707@colorado.edu>
2024-03-25 23:16:40 -0400

    added useAlert custom hook. added alert component. Contact page should be finished.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,46 +1,55 @@
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
-    
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.send(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: "Anders",
-        from_email: form.email,
-        to_email: "anewgard41@gmail.com",
-        message: form.message,
-      },
-      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    ).then(() => {
-      setIsLoading(false);
-      // TODO show success 
-      // todo hide alert
-      setForm({ name: "", email: "", message: "" });
-  }).catch((error) => {
-    setIsLoading(false);
-    console.log(error);
-    // TODO show error
-  });
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Anders",
+          from_email: form.email,
+          to_email: "anewgard41@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        showAlert({ show: true, text: "Message sent successfully", type: "success" });
+
+        setTimeout(() => {
+          setForm({ name: "", email: "", message: "" });
+          showAlert({ show: false, text: "", type: "danger" });
+        }, [3000]);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+        showAlert({ show: true, text: "Hm. Something went wrong.", type: "danger" });
+      });
   };
   const handleFocus = () => {};
   const handleBlur = () => {};
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get In Touch</h1>
         <form
