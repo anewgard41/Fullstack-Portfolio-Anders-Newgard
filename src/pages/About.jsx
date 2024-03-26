dc800f9e33b155c39bb7547cbf4656d2aab560a7
Anders Newgard <anne9707@colorado.edu>
2024-03-26 15:18:36 -0400

    began styling of about page. added constants directory and its index.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Anders</span>
+        </h1>
+
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+            Fullstack Developer ceaslessly on the hunt for excellent ideas and awesome people to work with. My unique blend of leadership, techical skills, and commitment to lifelong learning make me a valuable asset to any team. Unafraid to fail and always eager to learn. Let's build something awesome together. 
+          </p>
+        </div>
+
+        <div className='py-10 flex flex-col'>
+          <h3 className='subhead-text'>My Skills</h3>
+          <div className='mt-16 flex flex-wrap gap-12'>
+            
+          </div>
+        </div>
+    </section>
   )
 }
 
